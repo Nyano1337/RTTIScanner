eb7ee5463c42d327117fb5236f2730fa7f705305
Nyano <dison2233@126.com>
2024-10-09 03:11:35 +0800

    dont importing shits

--- RTTI/GCC.cs
+++ RTTI/GCC.cs
@@ -1,11 +1,6 @@
 ﻿using RTTIScanner.ClassExtensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Windows.UI.Composition;
 
 namespace __cxxabiv1
 {
